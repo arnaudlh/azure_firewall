d359b6c965428358edd0dfd3fcb96a5a4ee91071
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2019-09-06 14:05:52 +0800

    Removed samples

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "az_firewall_config" {
-  //exports ID, pip, config settings, rules? 
+
   value = {
       az_fw_name        = azurerm_firewall.az_firewall.name
       az_fw_id          = azurerm_firewall.az_firewall.id
