344725d198512701d19aa3fdd07d6877835fa291
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2019-10-01 14:36:14 +0800

    Refactoring to use diagnostics module

--- diagnostics.tf
+++ diagnostics.tf
@@ -1,34 +1,9 @@
-#Creates the diagnostics settings for the azure firewall
-resource "azurerm_monitor_diagnostic_setting" "az_firewall_diags" {
-
-name                             = "${azurerm_firewall.az_firewall.name}-diag"
-target_resource_id               = "${azurerm_firewall.az_firewall.id}"
-eventhub_name                    = "${var.diagnostics_map.eh_name}"
-eventhub_authorization_rule_id   = "${var.diagnostics_map.eh_id}/authorizationrules/RootManageSharedAccessKey"
-log_analytics_workspace_id       = "${var.log_analytics_workspace.id}"
-storage_account_id               = "${var.diagnostics_map.diags_sa}"
-log {
-
-        category =  "AzureFirewallApplicationRule"
-        retention_policy {
-            days   = var.opslogs_retention_period
-            enabled = true
-        }
-        }
-log {
-
-        category =  "AzureFirewallNetworkRule"
-        retention_policy {
-            days   = var.opslogs_retention_period
-            enabled = true
-        }
-        }
-metric {
-        category = "AllMetrics"
-
-        retention_policy {
-            days    = var.opslogs_retention_period
-            enabled = true
-                }
-    }
-}  
+module "diagnostics_asr" {
+  source = "git@ssh.dev.azure.com:v3/azure-terraform/Blueprints/module_diagnostics?ref=v0.1.1"
+  
+    name                            = azurerm_firewall.az_firewall.name
+    resource_id                     = azurerm_firewall.az_firewall.id
+    log_analytics_workspace_id      = var.log_analytics_workspace_id
+    diagnostics_map                 = var.diagnostics_map
+    diag_object                     = var.diagnostics_settings
+}
